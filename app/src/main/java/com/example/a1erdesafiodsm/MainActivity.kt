29c6f33236154299a3f22fbd7c95f0d9c96f9ab2
fershiiii <duranfernado349@gmail.com>
2026-08-15 14:07:45 -0600

    feat(add): agregando el menu de la aplicacion

--- app/src/main/java/com/example/a1erdesafiodsm/MainActivity.kt
+++ app/src/main/java/com/example/a1erdesafiodsm/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.a1erdesafiodsm
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnEjercicio1 = findViewById<Button>(R.id.btnEjercicio1)
+        val btnEjercicio2 = findViewById<Button>(R.id.btnEjercicio2)
+        val btnEjercicio3 = findViewById<Button>(R.id.btnEjercicio3)
+
+        // Botón para acceder al Ejercicio 1 (Promedio)
+        btnEjercicio1.setOnClickListener {
+            val intent = Intent(this, PromedioActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Botón para acceder al Ejercicio 2 (Salario)
+        btnEjercicio2.setOnClickListener {
+            val intent = Intent(this, SalarioActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Botón para acceder al Ejercicio 3 (Calculadora)
+        btnEjercicio3.setOnClickListener {
+            val intent = Intent(this, CalculadoraActivity::class.java)
+            startActivity(intent)
         }
     }
 }
